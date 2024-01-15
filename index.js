084cfd33581477f984484986ca021893d087901a
Jose <josexmoreno1998@gmail.com>
2024-01-15 11:21:21 -0300

    add cors allow all request

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import router from "./routes/routes.js";
+import cors from "cors";
 
 const port = 3000;
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
