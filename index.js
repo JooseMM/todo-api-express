21766070a9514f12fcbf63886ec252ed095acc12
Jose <josexmoreno1998@gmail.com>
2024-01-16 15:37:09 -0300

    path change

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api", router)
+app.use("/", router)
 app.listen(port, () => {
   console.log("Server running with nodemon on: " + port);
 });
--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ import { apiGetAll, apiGet, apiCreate, apiDelete } from "../controllers/apiContr
 const router = express.Router();
 
 router.get("/get-all", apiGetAll);
-router.get("/get-one", apiGet);
+router.get("/", apiGet);
 router.post("/", apiCreate);
 router.delete("/", apiDelete);
 
